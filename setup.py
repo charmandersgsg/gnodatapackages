500e9a099b3a356e300677b513901e16365bdda3
TH TAN <tan.tze.hoong@gmail.com>
2020-08-05 14:02:37 +0800

    updated colist for '

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setuptools.setup(
     name="gnodatapackages", # Replace with your own username
-    version="1.1.2",
+    version="1.1.4",
     author="charmandersgsg",
     author_email="charmandersgsg@gmail.com",
     description="data def packages for gno",
